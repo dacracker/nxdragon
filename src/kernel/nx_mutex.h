ce7a92c4ce34cecede335233a51a818810b1e8a1
Patrik Jeppsson <dacrackerx64@gmail.com>
2013-09-02 20:12:37 +0200

    Added a mutex impl. for Windows >= Vista

---
+++ src/kernel/nx_mutex.h
@@ -0,0 +1,53 @@
+/***************************************************************************\
+  This file is part of the NxDragon Game Engine.
+
+  Copyright 2013 Patrik Jeppsson
+  
+  NxDragon is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  NxDragon is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with NxDragon. If not, see <http://www.gnu.org/licenses/>.
+\***************************************************************************/
+
+#ifndef __NX_KERNEL_MUTEX_H__
+#define __NX_KERNEL_MUTEX_H__
+
+#include "nx_def.h"
+
+/** \ingroup NxKernel
+  * \defgroup nx_mutex nx_mutex
+  * \brief Mutex syncronization object 
+  * Lightweight synconization primitive. It's primary purpose
+  * is to protect a piece of code that must not be run by
+  * multiple threads at once. 
+  * It's possible to aquire the mutex lock recursively. 
+  * @{
+  */
+
+typedef struct nx_mutex_t nx_mutex; 
+
+/** Creates a new mutex object */
+NX_KERNEL_API nx_mutex* nx_mutex_create();
+
+/** Destroys the mutex */
+NX_KERNEL_API void nx_mutex_destroy(nx_mutex *self);
+
+/** Locks the mutex. Every call to this function must be paired with 
+  * corresponding call to nx_mutex_unlock() with the same mutex as argument 
+  */
+NX_KERNEL_API void nx_mutex_lock(nx_mutex *self);
+
+/** Unlocks the mutex. The mutex _must_ be locked before an attempt to unlock it is made. */
+NX_KERNEL_API void nx_mutex_unlock(nx_mutex *self);
+
+/** @} @} */
+
+#endif
---
+++ src/kernel/nx_mutex_win32.c
@@ -0,0 +1,58 @@
+/***************************************************************************\
+  This file is part of the NxDragon Game Engine.
+
+  Copyright 2013 Patrik Jeppsson
+  
+  NxDragon is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  NxDragon is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with NxDragon. If not, see <http://www.gnu.org/licenses/>.
+\***************************************************************************/
+
+#include "nx_mutex.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <stdlib.h>
+
+struct nx_mutex_t {
+	CRITICAL_SECTION critSection;
+};
+
+/*************************************************************/
+nx_mutex* nx_mutex_create()
+{
+	struct nx_mutex_t *self = malloc(sizeof(struct nx_mutex_t));
+	InitializeCriticalSectionEx(&self->critSection,
+								1200, /* HACK: Run this spin count through some profiling tests */
+								CRITICAL_SECTION_NO_DEBUG_INFO);
+	return self;
+}
+
+/*************************************************************/
+void nx_mutex_destroy(nx_mutex *self)
+{
+	DeleteCriticalSection(&self->critSection);
+	free(self);
+}
+
+/*************************************************************/
+void nx_mutex_lock(nx_mutex *self)
+{
+	EnterCriticalSection(&self->critSection);
+}
+
+/*************************************************************/
+void nx_mutex_unlock(nx_mutex *self)
+{
+	LeaveCriticalSection(&self->critSection);
+}
